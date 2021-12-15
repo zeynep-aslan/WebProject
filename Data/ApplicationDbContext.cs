75d40db2f78f54d6ee8e7a41c9fca54565cd212b
Zeynep Aslan <58036725+zeynep-aslan@users.noreply.github.com>
2021-12-15 23:39:13 +0300

    model was created.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MovieWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieWebsite.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Film> Film { get; set; }
+
+        public DbSet<Dil> Dil { get; set; }
+
+        public DbSet<Kategori> Kategori { get; set; }
+
+
+    }
+}
---
+++ Models/Film.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebsite.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        public string FilmAdi { get; set; }  // doldurmazsann hata vermez str old icin
+
+        public int? Yil { get; set; }  // deg.ici null olabilir sikinti yok, ? yoksa hata verir
+
+        public int? Uzunluk { get; set; }
+
+        public double? IMDB_Puan { get; set; }
+
+        public string Konu { get; set; }
+
+        public string Afis { get; set; }  // db den link cekilecek, str
+
+        public string Fragman { get; set; }  // link gelecek, str
+
+        public int KategoriId { get; set; }  // KategoriId fk 
+
+        public Kategori Kategori { get; set; }  // Kategori fk , Kategori nin pk i burada fk olarak kullaniyoruz
+
+        public int DilId { get; set; }  // fk
+
+        public Dil Dil { get; set; }  // 1.20.00
+
+
+        // oyuncuları burda tutamayız
+        // yorumlar tablosunda film id sini fk olarak tutucaz
+
+        // kategori-film arasinda pkfk iliskisi kurulacak
+    }
+}
